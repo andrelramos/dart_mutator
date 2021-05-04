f021dbcd48ef70f99fb99ea7cb863badad366ae6
André Ramos <andrel.ramos97@gmail.com>
2021-05-04 02:23:20 -0300

    :sparkles: Creating sandbox directory

--- lib/sandbox/sandbox.dart
+++ lib/sandbox/sandbox.dart
@@ -3,27 +3,58 @@
 //
 
 import 'dart:io';
-
+import 'package:dart_mutator/singleton.dart';
+import 'package:path/path.dart' as pathUtils;
 import 'package:dart_mutator/mutators/mutator.dart';
 
 class Sandbox {
-  List<File> _sourceCode;
-  Mutator _mutator;
+  Directory projectRoot;
+  Mutator mutator;
   late final Directory sandboxDir;
 
-  Sandbox(this._sourceCode, this._mutator) {
+  Sandbox(this.projectRoot, this.mutator) {
     sandboxDir = Directory(getMutatorSandboxPath());
   }
 
-  void create() async {
+  Future<void> create() async {
     // Create sandbox temporary file
     if(!await sandboxDir.exists()) {
-      sandboxDir.createTemp();
+      await sandboxDir.create(recursive: true);
+    }
+
+    // Copy project files to sandbox directory
+    await _copyFiles(projectRoot, sandboxDir);
+  }
+
+  Future<void> _copyFiles(Directory from, Directory to) async {
+    var files = await from.list().toList();
+
+    for (var path in files) {
+      // Get the path relative from sandbox directory
+      var relativePath = pathUtils.join(
+        to.path, pathUtils.relative(path.path, from: path.parent.path)
+      );
+
+      if(path is File) {
+        LogHandlerSingleton().d('Copying file from ${path.path} to $relativePath');
+        await path.copy(relativePath);
+      } else if(path is Directory) {
+        var sandboxRelativeDir = Directory(relativePath);
+
+        // Create relative subfolder on sandbox directory
+        if(!await sandboxRelativeDir.exists()) {
+          LogHandlerSingleton().d('Creating subfolder ${sandboxRelativeDir.path}');
+          await sandboxRelativeDir.create(recursive: true);
+        }
+
+        // Recursive call for copy files on subfolder
+        await _copyFiles(path, sandboxRelativeDir);
+      }
     }
   }
 
   String getMutatorSandboxPath() {
     final now = DateTime.now();
-    return "${Directory.systemTemp}/${_mutator.name}_${now.toIso8601String()}";
+    return pathUtils.joinAll([Directory.systemTemp.path, now.toIso8601String(), mutator.name]);
   }
 }
---
+++ test/sandbox/sandbox_test.dart
@@ -0,0 +1,59 @@
+import 'dart:io';
+import 'package:dart_mutator/mutators/arithmetic_operator.dart';
+import 'package:dart_mutator/sandbox/sandbox.dart';
+import 'package:test/test.dart';
+import 'package:path/path.dart';
+
+const mainSource = '''
+  void main() {
+    // checking reality
+    if (3 < 4) print('ok');
+    if (3 < 4) print('ok');
+    if (5 == 5) print('ok');
+  }
+  ''';
+
+const mainTestSource = '''
+  import 'package:test/test.dart';
+
+  void main() {
+    test('generic test', () async {
+      expect(true, true);
+    });
+  }
+  ''';
+
+void main() async {
+  // Creating a fake project directory
+  final rootPath = Directory('${Directory.systemTemp.path}').createTempSync('sandbox_test');
+  final srcPath = Directory('${rootPath.path}/src')..createSync();
+  final testPath = Directory('${rootPath.path}/test')..createSync();
+
+  final mainFile = File('${srcPath.path}/main.dart')
+    ..createSync()
+    ..writeAsStringSync(mainSource);
+
+  final mainTestFile = File('${testPath.path}/main_test.dart')
+    ..createSync()
+    ..writeAsStringSync(mainTestSource);
+    
+  // Run tests
+  test('creating sandbox environment', () async {
+    var testSandbox = Sandbox(rootPath, ArithmeticOperatorMutator());
+    await testSandbox.create();
+
+    for(var sandboxPath in await testSandbox.sandboxDir.list().toList()) {
+      var relativeSandboxPath = relative(sandboxPath.path, from: sandboxPath.parent.path);
+
+      var projectPath = Directory(join(
+        testSandbox.projectRoot.path, 
+        relativeSandboxPath
+      ));
+
+      expect(true, await projectPath.exists());
+      expect(true, await sandboxPath.exists());
+    }
+  });
+
+  // TODO Remove fake project directory
+}
